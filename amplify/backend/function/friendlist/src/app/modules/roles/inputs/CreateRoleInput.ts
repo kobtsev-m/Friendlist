b369a6e5b4fab4b6f13891c19e0ffada7dc84d0c
kobtsev-m <kobtsev.info@gmail.com>
2022-09-20 22:42:48 +0700

    test: default lambda function

--- amplify/backend/function/friendlist/src/app/modules/roles/inputs/CreateRoleInput.ts
+++
@@ -1,7 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
-export class CreateRoleInput {
-  @Field() value: string;
-  @Field() description: string;
-}
--- amplify/backend/function/friendlist/src/app/modules/users/inputs/CreateUserInput.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType()
-export class CreateUserInput {
-  @Field() email: string;
-  @Field() password: string;
-  @Field() firstName: string;
-  @Field() lastName: string;
-}
