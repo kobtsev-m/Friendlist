dec7b6122943d5914e5be72edb07d9313b8d57d4
kobtsev-m <kobtsev.info@gmail.com>
2022-09-24 19:01:57 +0700

    fix: backend bash script execute permissions (5)

--- amplify/backend/function/friendlist/src/amplify-push.sh
+++ amplify/backend/function/friendlist/src/amplify-push.sh
@@ -6,7 +6,11 @@ yarn migration:run
 cp yarn.lock yarn.lock.orig
 rm -rf node_modules
 
+cd ../../../../../
+
 echo | amplify push 2>&1 | tee -a amplify-push.log
+
+cd amplify/backend/function/friendlist/src
 
 rm -rf package-lock.json node_modules
 mv yarn.lock.orig yarn.lock
